972d45747946eefd609f1a2cae695b5023d8f4d5
twolaw <twolaw@free.fr>
2018-04-19 15:22:24 +0200

    0.3.7

--- service.py
+++ service.py
@@ -54,7 +54,6 @@ class Main:
             if ((xbmc.getCondVisibility("Window.IsActive(Videos)") and xbmc.getCondVisibility("Window.Is(Videos)")) or (xbmc.getCondVisibility("Window.IsActive(MovieInformation)") and xbmc.getCondVisibility("Window.Is(MovieInformation)"))) and not xbmc.getCondVisibility("Container.Scrolling"):
                 self.selecteditem = xbmc.getInfoLabel("ListItem.DBID")
                 if (self.selecteditem and self.selecteditem != self.previousitem):
-                    #xbmc.executebuiltin('Notification(Hello World,test,500)')
                     self.previousitem = self.selecteditem
                     
                     if (xbmc.getInfoLabel("ListItem.DBID") > -1 and not xbmc.getCondVisibility("ListItem.IsFolder")) and xbmc.getInfoLabel("ListItem.Duration") and int(float(xbmc.getInfoLabel("ListItem.Duration"))) > 0:
@@ -62,11 +61,10 @@ class Main:
                         self.dbid = xbmc.getInfoLabel("ListItem.DBID")
                         self.set_duration_values()
             else:
-                my_container_id = xbmc.getInfoLabel("Window(Home).Property(Duration.WidgetContainerId)")
-                my_container_window = xbmc.getInfoLabel("Window(Home).Property(Duration.WidgetContainerWindowName)")
+                my_container_id = xbmc.getInfoLabel("Window(Home).Property(Durations.WidgetContainerId)")
+                my_container_window = xbmc.getInfoLabel("Window(Home).Property(Durations.WidgetContainerWindowName)")
                 
                 if (my_container_id and my_container_window and (xbmc.getCondVisibility("Control.HasFocus("+my_container_id+")") and xbmc.getCondVisibility("Window.IsActive("+my_container_window+")") and xbmc.getCondVisibility("Window.Is("+my_container_window+")")) and not xbmc.getCondVisibility("Window.IsActive(Videos)") and not xbmc.getCondVisibility("Window.IsActive(MovieInformation)")) and not xbmc.getCondVisibility("Container("+my_container_id+").Scrolling"):
-                    #xbmc.executebuiltin('Notification(Hello World,'+my_container_id+' '+my_container_window+',500)')
                     self.selecteditem = xbmc.getInfoLabel("Container("+my_container_id+").ListItem.DBID")
                     if (self.selecteditem and self.selecteditem != self.previousitem):
                         self.previousitem = self.selecteditem
@@ -78,21 +76,19 @@ class Main:
 
     def set_duration_values(self):
         log('Converts: '+self.duration+' min')
-        #xbmc.executebuiltin('Notification(Hello World,'+self.duration+' '+self.dbid+',250)')
-        
         # HoursMinutes
         hours_and_minutes = get_hours_and_minutes(self.duration)
-        xbmc.executebuiltin('SetProperty(Durations.HoursMinutes,'+hours_and_minutes+',home)')
+        xbmc.executebuiltin('SetProperty(Durations.HoursMinutes,"'+hours_and_minutes+'",home)')
         # Hours
         hours_only = get_hours_only(self.duration)
-        xbmc.executebuiltin('SetProperty(Durations.Hours,'+hours_only+',home)')
+        xbmc.executebuiltin('SetProperty(Durations.Hours,"'+hours_only+'",home)')
         # Minutes
         minutes_only = get_minutes_only(self.duration)
-        xbmc.executebuiltin('SetProperty(Durations.Minutes,'+minutes_only+',home)')
+        xbmc.executebuiltin('SetProperty(Durations.Minutes,"'+minutes_only+'",home)')
         # DBID
-        xbmc.executebuiltin('SetProperty(Durations.DBID,'+self.dbid+',home)')
+        xbmc.executebuiltin('SetProperty(Durations.DBID,"'+self.dbid+'",home)')
         # InputDurationMinutes
-        xbmc.executebuiltin('SetProperty(Durations.InputDurationMinutes,'+self.duration+',home)')
+        xbmc.executebuiltin('SetProperty(Durations.InputDurationMinutes,"'+self.duration+'",home)')
 
 if (__name__ == "__main__"):
     Main()
